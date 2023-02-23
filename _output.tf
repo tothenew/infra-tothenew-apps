0d2866e59818e8ea957cf812d461c189a82358a3
Chhavi Sharma <chhavi.sharma@tothenew.com>
2023-02-23 12:38:53 +0000

    cleaned up resources

--- _output.tf
+++ _output.tf
@@ -4,9 +4,3 @@ output "core-service-role" {
 output "shared-apps-helm-integration-role" {
     value =   module.secrets-store-csi.iam_role_arn
 }
-output "elasticsearch_ip_addr" {
-  value = join(":",[aws_instance.elastic_nodes.private_ip,"9200"])
-}
-output "kibana_ip_addr" {
-  value = join(":",[aws_instance.kibana.private_ip,"5601"])
-}
--- elasticsearch.tf
+++ elasticsearch.tf
@@ -1,14 +1,13 @@
 module "elasticsearch" {
   source                       = "git::https://github.com/tothenew/terraform-aws-elasticsearch.git?ref=v0.0.2"
-  create_aws_elasticsearch     = false
-  create_aws_ec2_elasticsearch = true
+  create_aws_elasticsearch     = true
+  create_aws_ec2_elasticsearch = false
   instance_count               = local.workspace.elasticsearch.instance_count
   instance_type                = local.workspace.elasticsearch.instance_type
   project_name_prefix          = local.workspace.environment_name
-  security_group_ids           = [aws_security_group.es-sg.id]
   subnet_ids                   = data.aws_subnets.private.ids
   volume_size                  = local.workspace["elasticsearch"]["volume_size"]
   vpc_id                       = data.aws_vpc.selected.id
-  key_name                     = local.workspace["key_name"]
+  #key_name                     = local.workspace["key_name"]
   volume_type                  = local.workspace["elasticsearch"]["volume_type"]
 }
